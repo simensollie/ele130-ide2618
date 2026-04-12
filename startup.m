de5023945927bb18b980886931f2e64f1a8b4c91
Simen Sollie <simen@sollie.io>
2026-04-12 17:05:20 +0200

    Startup-fil for å sette LaTeX teksttolker

---
+++ startup.m
@@ -0,0 +1,8 @@
+% Setter LaTeX som standard teksttolker for figurer
+set(groot, 'defaultTextInterpreter', 'latex')
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex')
+set(groot, 'defaultLegendInterpreter', 'latex')
+
+% Legger til MineFunksjoner og måledata i path
+addpath('MineFunksjoner')
+addpath('Prosjekt00_TestOppkopling')
