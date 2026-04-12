46b7b4a1800809c311534373d8f21db569d20ab6
Simen Sollie <simen@sollie.io>
2026-04-12 18:17:54 +0200

    6.3 Filtrering av intensitetsprofil i bilde, måleserie 2

--- Prosjekt02_Filtrering/Måleserier/Serie2_FiltrerLena/Serie2_FiltrerLena.m
+++ Prosjekt02_Filtrering/Måleserier/Serie2_FiltrerLena/Serie2_FiltrerLena.m
@@ -3,3 +3,23 @@ clear; close all
 load('Serie2_Bilde.mat')
 u = Lys;  % intensitetsprofilen i bilde
 
+% FrekvensSpekter(t, u);
+
+fc = 0.8;
+tau = 1/(2*pi*fc);
+
+% Lavpassfiltrering i 1. orden
+u_f = LavpassFilter(t, u, tau=tau);
+
+% Lavpassfiltrering i 3. orden
+u1_f = LavpassFilter(t, u,    tau=tau/3);   % 1. orden
+u2_f = LavpassFilter(t, u1_f, tau=tau/3);   % 2. orden
+u3_f = LavpassFilter(t, u2_f, tau=tau/3);   % 3. orden
+
+% Sammenligning av filtreringene
+figure
+plot(t, u, 'b', t, u_f, 'r', t, u3_f, 'g')
+grid on
+legend('$\{u_k\}$', '$\{u_{f,k}\}$', '$\{u_{3f,k}\}$')
+title('Filtrering av intensitetsprofil i bilde')
+xlabel('tid [s]')
